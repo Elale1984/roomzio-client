4253372d3f7dd4cbc59e789aa16ac39ab9f25ba6
Edward <edward.gratefuldead1@gmail.com>
2023-11-18 15:42:52 -0600

    Finished navigation and SideBar nav with collapse

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import SideBar from './components/sideBar/SideBar';
+import Facility from './pages/Facility';
+import Login from './pages/Login';
+import Organization from './pages/Organization';
+import Register from './pages/Register';
+import Room from './pages/Room';
+import Welcome from './pages/Welcome';
+import Wing from './pages/Wing';
+
 
 const  App = () => {
   return (
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+    <SideBar>
+      <Routes>
+        <Route path="/welcome" element={<Welcome />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/organization" element={<Organization />}/>
+        <Route path="/facility" element={<Facility />}/>
+        <Route path="/wing" element={<Wing />}/>
+        <Route path="/room" element={<Room />}/>
+      </Routes>
+    </SideBar>
+    </BrowserRouter>
   );
 }
 
