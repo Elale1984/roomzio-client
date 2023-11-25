401ba35d18bf7541f6f221b8ffe26c0cd46bb7ef
Edward Lale <edward.gratefuldead1@gmail.com>
2023-11-24 21:24:34 -0600

    starting on landing-page

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './Welcome.css'
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
-  )
+    <div className="welcome-container">
+      <div className="welcome-container-left"></div>
+      <div className="welcome-container-right"></div>
+    </div>
+    )
 }
 
 export default Welcome
